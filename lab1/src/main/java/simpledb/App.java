7afbcf25993fb02c267835eec9bcb108640857a3
cpchung <chakpongchung@gmail.com>
2018-11-19 21:22:11 -0500

    from ant to maven

---
+++ lab1/src/main/java/simpledb/App.java
@@ -0,0 +1,13 @@
+package simpledb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
