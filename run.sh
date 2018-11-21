8efa57a71d449bdb9321970b473a9b13ccbe2fed
cpchung <chakpongchung@gmail.com>
2018-11-20 19:46:07 -0500

    compatibility

--- run.sh
+++
@@ -1,6 +0,0 @@
-# mvn archetype:generate -DgroupId=simpledb -DartifactId=lab1 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-mvn archetype:generate -DgroupId=simpledb -DartifactId=lab2 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-# mvn archetype:generate -DgroupId=simpledb -DartifactId=lab3 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-# mvn archetype:generate -DgroupId=simpledb -DartifactId=lab4 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-# mvn archetype:generate -DgroupId=simpledb -DartifactId=lab5 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
-# mvn archetype:generate -DgroupId=simpledb -DartifactId=lab6 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
